13efdc609049a9f54d67935c3eae05ffa29be381
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-09-12 20:12:55 +0200

    klar med programmet

--- potens.py
+++ potens.py
@@ -1,1 +1,17 @@
-print("hej världen")
+bas_str = input("Skriv potens bas: ") 
+bas=int(bas_str)
+
+exp_str = input("Skriv exponenten: ") 
+exp=int(exp_str)
+
+potens=1
+
+for x in range(0, exp):
+    potens = potens * bas
+
+print(f"Potensens värde är: {potens}")
+
+
+
+
+
